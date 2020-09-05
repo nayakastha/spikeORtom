d2f3c52cbae604ded32d21709862c0bf32f640c1
Ankitasareen <65178427+Ankitasareen@users.noreply.github.com>
2020-09-05 23:51:31 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,18 @@
+function myfunction()
+{var request = new XMLHttpRequest();
+
+request.open('GET', "https://xcusemaker.herokuapp.com/getx/", true);
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.request);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((MESSAGE) => {
+      console.log(MESSAGE);
+      console.log("Ank");
+    })
+  } else {
+    console.log('error');
+  }
+}
+};
