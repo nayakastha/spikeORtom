5883d0a26d1a61f27939b2ed3b84df3f9b6d41e0
Chinmay Kabi <chinmay.kabi@live.com>
2020-09-06 18:18:54 +0530

    minor changes

--- web/website/script.js
+++ web/website/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function myfunction() {
     document.getElementById("excuse").innerHTML = "Loading";
     const Url = 'http://35.208.101.72/getx/';
@@ -33,42 +32,4 @@ function longLoad() {
             document.getElementById("excuse").innerHTML = res.excuses[0];
 
         });
-
-=======
-function myfunction() {
-    document.getElementById("excuse").innerHTML = "Loading";
-    const Url = 'http://34.121.79.151/getx/';
-    const othePram = {
-        headers: {
-            "content-type": "application/json; charset=UTF-8"
-        },
-        method: "GET"
-    };
-    fetch(new URL(Url))
-        .then(data => data.json())
-        .then(res => {
-            document.getElementById("excuse").innerHTML = res.MESSAGE;
-        });
-
-}
-
-function longLoad() {
-    document.getElementById("excuse").innerHTML = "Loading";
-
-    const Url = 'http://34.121.79.151/excuse/gen';
-    const othePram = {
-        headers: {
-            "content-type": "application/json; charset=UTF-8"
-        },
-        method: "GET"
-    };
-    fetch(new URL(Url))
-        .then(data => data.json())
-        .then(res => {
-            console.log(res);
-            document.getElementById("excuse").innerHTML = res.excuses[0];
-
-        });
-
->>>>>>> upstream/master
 }
